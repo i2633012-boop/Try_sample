ed587fec9549544baf98627c8f52665998e58a4b
dinesh  bahadur malla <dnsmalla@gmail.com>
2026-05-22 09:23:47 +0900

    Initial commit

---
+++ Source/MyProject/MyClass_noparent.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyClass_noparent.h"
+
+MyClass_noparent::MyClass_noparent()
+{
+}
+
+MyClass_noparent::~MyClass_noparent()
+{
+}
